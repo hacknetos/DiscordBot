a541ec53c1065a67f0261d455d597281c7b78f39
Arne Rosenberger <arnoberg04@gmail.com>
2022-04-18 16:08:41 +0200

    nn

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -2,12 +2,16 @@
 using System.IO;
 using System.Threading.Tasks;
 
+
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
+using Discord;
 using Discord.Addons.Hosting;
 using Discord.WebSocket;
+using Discord.Commands;
 
 namespace Bot
 {
@@ -20,29 +24,48 @@ namespace Bot
                 {
                     var configuration = new ConfigurationBuilder()
                     .SetBasePath(Directory.GetCurrentDirectory())
-                    .AddJsonFile("appsettings.json",false,true)
+                    .AddJsonFile("appsettings.json", false, true)
                     .Build();
 
                     x.AddConfiguration(configuration);
                 })
-                .ConfigureLogging(x => {
+                .ConfigureLogging(x =>
+                {
 
                     x.AddConsole();
                     x.SetMinimumLevel(LogLevel.Debug);
 
-                    
+
                 })
-                .ConfigureDiscordHost<DiscordSocketClient>((Context , config) =>
+                .ConfigureDiscordShardedHost((Context, config) =>
                 {
-                    config.SoketConfig = new DiscordSocketConfig
+                    config.SocketConfig = new DiscordSocketConfig
                     {
-                        LogLevel = Discord.Debug,
-                        AlwaysDownloadUsers = fasle,
+                        LogLevel = LogSeverity.Debug,
+                        AlwaysDownloadUsers = false,
                         MessageCacheSize = 100,
 
                     };
-                    config.token = Context.Configuration["Token"]
+                    config.Token = Context.Configuration["Token"];
                 })
+                .UseCommandService((Context, Config) =>
+                {
+                    Config.CaseSensitiveCommands = false;
+                    Config.LogLevel = LogSeverity.Debug;
+                    Config.DefaultRunMode = RunMode.Async;
+                })
+                .ConfigureServices((Context, services) =>
+                {
+                    //services
+                    //    .AddHostedService<CommandHandler>();
+                })
+                .UseConsoleLifetime();
+
+            var host = builder.Build();
+            using (host)
+            {
+                await host.RunAsync();
+            }
         }
     }
 }
