0e69d08f56ee36a662f34029a53dc9a3731ec142
hacknetos <76149789+hacknetos@users.noreply.github.com>
2022-05-05 11:59:26 +0200

    PAsst

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,188 +1,66 @@
 ﻿using System;
-using System.Reflection;
-using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
 using Discord;
+using Discord.Net;
 using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
 using Discord.WebSocket;
-
-class Program
+namespace DoscordBot
 {
-    // Program entry point
-    static Task Main(string[] args)
+    public class Program : ModuleBase<SocketCommandContext>
     {
-        // Call the Program constructor, followed by the 
-        // MainAsync method and wait until it finishes (which should be never).
-        return new Program().MainAsync();
-    }
+        static void Main(string[] args)
+        => new Program()
+        .RunBotAsync()
+        .GetAwaiter()
+        .GetResult();
 
-    private readonly DiscordSocketClient _client;
-
-    // Keep the CommandService and DI container around for use with commands.
-    // These two types require you install the Discord.Net.Commands package.
-    private readonly CommandService _commands;
-    private readonly IServiceProvider _services;
-
-    private Program()
-    {
-        _client = new DiscordSocketClient(new DiscordSocketConfig
+        public static DiscordSocketClient _client;
+        private CommandService _commands;
+        public IServiceProvider _services;
+        public async Task RunBotAsync()
         {
-            // How much logging do you want to see?
-            LogLevel = LogSeverity.Info,
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+            string token = Console.ReadLine();
+            _client.Log += Client_log;
+            await RegisterCommandAsync();
+           await _client.LoginAsync(TokenType.Bot, token);
+           await _client.StartAsync();
 
-            // If you or another service needs to do anything with messages
-            // (eg. checking Reactions, checking the content of edited/deleted messages),
-            // you must set the MessageCacheSize. You may adjust the number as needed.
-            MessageCacheSize = 50,
-
-            // If your platform doesn't have native WebSockets,
-            // add Discord.Net.Providers.WS4Net from NuGet,
-            // add the `using` at the top, and uncomment this line:
-            //WebSocketProvider = WS4NetProvider.Instance
-        });
-
-        _commands = new CommandService(new CommandServiceConfig
-        {
-            // Again, log level:
-            LogLevel = LogSeverity.Info,
-
-            // There's a few more properties you can set,
-            // for example, case-insensitive commands.
-            CaseSensitiveCommands = false,
-        });
-
-        // Subscribe the logging handler to both the client and the CommandService.
-        _client.Log += Log;
-        _commands.Log += Log;
-
-        // Setup your DI container.
-        _services = ConfigureServices();
-
-    }
-
-    // If any services require the client, or the CommandService, or something else you keep on hand,
-    // pass them as parameters into this method as needed.
-    // If this method is getting pretty long, you can seperate it out into another file using partials.
-    private static IServiceProvider ConfigureServices()
-    {
-        var map = new ServiceCollection();
-            // Repeat this for all the service classes
-            // and other dependencies that your commands might need.
-            //.AddSingleton(new SomeServiceClass());
-
-        // When all your required services are in the collection, build the container.
-        // Tip: There's an overload taking in a 'validateScopes' bool to make sure
-        // you haven't made any mistakes in your dependency graph.
-        return map.BuildServiceProvider();
-    }
-
-    // Example of a logging handler. This can be re-used by addons
-    // that ask for a Func<LogMessage, Task>.
-    private static Task Log(LogMessage message)
-    {
-        switch (message.Severity)
-        {
-            case LogSeverity.Critical:
-            case LogSeverity.Error:
-                Console.ForegroundColor = ConsoleColor.Red;
-                break;
-            case LogSeverity.Warning:
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                break;
-            case LogSeverity.Info:
-                Console.ForegroundColor = ConsoleColor.White;
-                break;
-            case LogSeverity.Verbose:
-            case LogSeverity.Debug:
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-                break;
-        }
-        Console.WriteLine($"{DateTime.Now,-19} [{message.Severity,8}] {message.Source}: {message.Message} {message.Exception}");
-        Console.ResetColor();
-
-        // If you get an error saying 'CompletedTask' doesn't exist,
-        // your project is targeting .NET 4.5.2 or lower. You'll need
-        // to adjust your project's target framework to 4.6 or higher
-        // (instructions for this are easily Googled).
-        // If you *need* to run on .NET 4.5 for compat/other reasons,
-        // the alternative is to 'return Task.Delay(0);' instead.
-        return Task.CompletedTask;
-    }
-
-    private async Task MainAsync()
-    {
-        // Centralize the logic for commands into a separate method.
-        await InitCommands();
-
-        // Get token from EnvironmentVariable DiscordToken
-        // if token null or empty ask for new token and set the token as EnvironmentVariable
-        
-        // HACKNET
-        // habe keine ahnung eiw environment variables richtig gehen...
-        // der code hier fragt immer nach nem token...
-        string token = Environment.GetEnvironmentVariable("DiscordToken");
-        if (string.IsNullOrEmpty(token))
-        {
-            Console.WriteLine("Please enter your bot token:");
-            token = Console.ReadLine();
-            Environment.SetEnvironmentVariable("DiscordToken", token);
+            await _client.SetGameAsync("use sp.");
+            await Task.Delay(-1);
+            
         }
 
-
-        // Login and connect.
-        await _client.LoginAsync(TokenType.Bot, token);
-        await _client.StartAsync();
-
-        // Wait infinitely so your bot actually stays connected.
-        await Task.Delay(Timeout.Infinite);
-    }
-
-    private async Task InitCommands()
-    {
-        // Either search the program and add all Module classes that can be found.
-        // Module classes MUST be marked 'public' or they will be ignored.
-        // You also need to pass your 'IServiceProvider' instance now,
-        // so make sure that's done before you get here.
-        await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
-        // Or add Modules manually if you prefer to be a little more explicit:
-        //await _commands.AddModuleAsync<SomeModule>(_services);
-        // Note that the first one is 'Modules' (plural) and the second is 'Module' (singular).
-
-        // Subscribe a handler to see if a message invokes a command.
-        _client.MessageReceived += HandleCommandAsync;
-    }
-    
-private async Task HandleCommandAsync(SocketMessage arg)
-    {
-        // Bail out if it's a System Message.
-        var msg = arg as SocketUserMessage;
-        if (msg == null) return;
-
-        // We don't want the bot to respond to itself or other bots.
-        if (msg.Author.Id == _client.CurrentUser.Id || msg.Author.IsBot) return;
-
-        // Create a number to track where the prefix ends and the command begins
-        int pos = 0;
-        // Replace the '!' with whatever character
-        // you want to prefix your commands with.
-        // Uncomment the second half if you also want
-        // commands to be invoked by mentioning the bot instead.
-        if (msg.HasStringPrefix(prefix, ref pos) || msg.HasMentionPrefix(_client.CurrentUser, ref pos) )
+        public async Task RegisterCommandAsync()
         {
-            // Create a Command Context.
-            var context = new SocketCommandContext(_client, msg);
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(System.Reflection.Assembly.GetEntryAssembly(), _services);
+        }
+        public async Task HandleCommandAsync(SocketMessage msg)
+        {
+            var msg2 = msg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, msg2);
 
-            // Execute the command. (result does not indicate a return value, 
-            // rather an object stating if the command executed successfully).
-            var result = await _commands.ExecuteAsync(context, pos, _services);
-
-            // Uncomment the following lines if you want the bot
-            // to send a message if it failed.
-            // This does not catch errors from commands with 'RunMode.Async',
-            // subscribe a handler for '_commands.CommandExecuted' to see those.
-            if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
-                await msg.Channel.SendMessageAsync(result.ErrorReason);
+            if (msg.Author.IsBot) return;
+            int argPos = 0;
+            if (msg2.HasStringPrefix("sp.",ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+                if (result.Error.Equals(CommandError.UnmetPrecondition)) await msg2.Channel.SendMessageAsync(result.ErrorReason);
+            }
+        }
+        public Task Client_log(LogMessage message) {
+            Console.WriteLine(message);
+            return Task.CompletedTask;
         }
     }
+
+
 }
---
+++ DiscordBot/Services/Commands.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace DiscordBot.Services
+{
+    public class MoederatorCommands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task ping()
+        {
+            await ReplyAsync("pong!");
+        }
+        [Command("Kick")]
+        [Discord.Commands.RequireUserPermission(Discord.GuildPermission.KickMembers),]
+        public async Task kick(SocketGuildUser user = null)
+        {
+            if(user == null)
+            {
+                await Context.Channel.SendMessageAsync("Kein User Angegeben");
+                return;
+            }
+            if(user == Context.User)
+            {
+                await Context.Channel.SendMessageAsync("man Kann sich nicht selber Kicken");
+                return;
+            }
+            await user.KickAsync();
+            await Context.Channel.SendMessageAsync("ich habe @"+ user.Username +" / "+ user.DisplayName +" Gekickt");
+        }
+    }
+}
